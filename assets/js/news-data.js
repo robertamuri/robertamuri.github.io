3ed7c2479e65b6c091413f3bf0e5266ee655687f
Roberta Muri <167425637+robertamuri@users.noreply.github.com>
2026-06-05 11:04:06 -0700

    Update news items with new dates and text revisions

--- assets/js/news-data.js
+++ assets/js/news-data.js
@@ -1,18 +1,32 @@
 const newsItems = [
   {
-    date: "Apr. 5",
+    date: "Apr. 5, 2026",
     datetime: "2026-04-05",
-    text: `The paper <em>Firms Under Water: Floods, Adaptation and Performance</em> has been selected by IAERE among the top four contributions in Environmental Economics by young economists. I will present it at the Annual Meeting of the Italian Economic Society in Cagliari in October 2026, where the final decision for the Young Economist Award will be made.`
+    text: `My paper <em>Firms Under Water: Floods, Adaptation and Performance</em> was selected by IAERE among the top four contributions in Environmental Economics by young economists. I will present the paper at the Annual Meeting of the Italian Economic Society in Cagliari in October 2026, where the final recipient of the Young Economist Award will be announced.`
   },
+
   {
-    date: "Feb. 22",
+    date: "Feb. 22, 2026",
     datetime: "2026-02-22",
-    text: `I have just landed in Los Angeles. I will be visiting Caltech, advised by Hannah Druckenmiller, until July 2026.`
+    text: `I arrived in Los Angeles to begin my visiting period at Caltech, where I will be advised by Hannah Druckenmiller until July 2026.`
   },
+
   {
-    date: "Feb. 12",
+    date: "Feb. 12, 2026",
     datetime: "2026-02-12",
-    text: `I will present the paper <em>Firms Under Water: Floods, Adaptation and Performance</em> at the IAERE Annual Conference in Trento.`
+    text: `I presented my paper <em>Firms Under Water: Floods, Adaptation and Performance</em> at the IAERE Annual Conference in Trento.`
+  },
+
+  {
+    date: "Sept. 15, 2025",
+    datetime: "2025-09-15",
+    text: `I presented the working paper <em>Classifying Hydrological Risk Adaptation Policies with Large Language Models: The HYDROADAPT Dataset</em> at the workshop <em>Mapping Environmental Vulnerabilities: Data, Space and Risk</em> at the University of Roma Tre.`
+  },
+
+  {
+    date: "Jun. 26, 2025",
+    datetime: "2025-06-26",
+    text: `My paper <a href="https://link.springer.com/article/10.1007/s10640-025-01013-w" target="_blank" rel="noopener"><em>Firms Under Water: Floods, Adaptation and Performance</em></a> was published in <em>Environmental and Resource Economics</em>.`
   }
 ];
 
